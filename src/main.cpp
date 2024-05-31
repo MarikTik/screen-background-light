6a42112ef4825166541743308d7a6ba44f84e476
MarikTik <mtik.philosopher@gmail.com>
2024-05-31 14:13:30 -0700

    update main.cpp with ota_server setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 #include "wifi.h"
 #include "tcp_server.h"
-
+#include "ota_server.h"
 
 static wifi_connection connection(WIFI_SSID, WIFI_PASSWORD, HOSTNAME);
-static tcp_server server(SERVER_PORT);
+static tcp_server worker_server(HOSTNAME, SERVER_PORT);
+static ota_server ota_upload_server(HOSTNAME, OTA_SERVER_PORT);
 
 void setup() {
   Serial.begin(BAUD_RATE);
   connection.begin();
-  server.begin();
+  worker_server.begin();
+  ota_upload_server.begin();
 }
 
 void loop(){
   connection.update();
-  server.update();
+  worker_server.update();
+  ota_upload_server.update();
 }
